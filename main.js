70f278817385e9301137ad3f2ce522aebebac8f5
MaxDeale <maxericdeale@gmail.com>
2020-07-17 15:48:44 +0200

    second

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
+let totalCalls = [];
+
+function myLoad() {
+  if (sessionStorage.getItem("hasCodeRunBefore") === null) {
+    localStorage.setItem("totalCalls", [
+      {
+        "Mark Johnson": 12,
+        "Jacob Smith": 34,
+        "Larry Goberson": 62,
+        "Mary Henders": 23,
+        "Sally Smith": 12,
+      },
+    ]);
+
+    sessionStorage.setItem("hasCodeRunBefore", true);
+  } else {
+    // otherwise set the array to whatever is in the session storage
+    totalCalls = JSON.parse(sessionStorage.getItem("totalCalls"));
+  }
+}
+
 const getTotalCalls = () => {
   fetch("https://www.breakingbadapi.com/api/characters")
     .then((res) => {
       return res.json();
     })
     .then((resData) => {
-      console.log(resData);
+      localStorage.setItem("totalCalls", resData);
+      totalCalls = resData;
+      console.log(totalCalls);
     });
 };
-
-let currentData = [];
-
-
-
-
-localStorage.setItem("totalCalls", [
-  {
-    "Mark Johnson": 12,
-    "Jacob Smith": 34,
-    "Larry Goberson": 62,
-    "Mary Henders": 23,
-    "Sally Smith": 12,
-  },
-]);
